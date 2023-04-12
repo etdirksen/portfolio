6b3b8740e0afdcc96aef68804d49fa10a500572b
Ethan Dirksen <etdirksen@gmail.com>
2023-04-12 15:51:55 -0500

    Refactoring code, making it modular

---
+++ docs/static/js/generateNavbarAndFooter.js
@@ -0,0 +1,65 @@
+const navbarPagesDict = [
+    ["Home","index.html"], 
+    ["Resume","resume.html"],
+    ["Porfolio","porfolio.html"],
+    ["About", null]
+];
+
+function createNavbarContainer(nameOfParentElement) {
+    let parentElement = document.getElementById(nameOfParentElement);
+
+    let navbarContainer = document.createElement("div");
+    navbarContainer.id = "navbarContainer";
+    navbarContainer.className = "container-fluid";
+    parentElement.appendChild(navbarContainer);
+};
+
+function createNavbarNavElement(nameOfParentElement) {
+    let parentElement = document.getElementById(nameOfParentElement);
+
+    let navbarDiv = document.createElement("nav");
+    navbarDiv.id = "navbar";
+    navbarDiv.style = `--bs-breadcrumb-divider: "//";`;
+    navbarDiv.setAttribute("aria-label", "breadcrumb");
+    parentElement.appendChild(navbarDiv);
+};
+
+function createNavbarOrderedList(nameOfParentElement) {
+    let parentElement = document.getElementById(nameOfParentElement);
+
+    let navbarOrderedList = document.createElement("ol");
+    navbarOrderedList.id = "navbar-ol-element"
+    navbarOrderedList.className = "breadcrumb";
+    parentElement.appendChild(navbarOrderedList);
+};
+
+function createNavbarListElement(nameOfParentElement, text, link) {
+    if (link != null) {
+        let parentElement = document.getElementById(nameOfParentElement);
+
+        let navbarListElement = document.createElement("li");
+        navbarListElement.className = "breadcrumb-item";
+        
+        let navbarListElementLink = document.createElement("a");
+        navbarListElementLink.href = link
+        navbarListElementLink.textContent = text;
+
+        parentElement.appendChild(navbarListElement);
+        navbarListElement.appendChild(navbarListElementLink);
+    }
+
+    else {
+        let parentElement = document.getElementById(nameOfParentElement);
+
+        let navbarListElement = document.createElement("li");
+        navbarListElement.className = "breadcrumb-item disabled";
+
+        let navbarListElementLink = document.createElement("a");
+        navbarListElementLink.textContent = text;
+        
+        parentElement.appendChild(navbarListElement);
+        navbarListElement.appendChild(navbarListElementLink);
+    };
+};
+
+export {navbarPagesDict, createNavbarContainer, createNavbarNavElement, createNavbarOrderedList, createNavbarListElement};
---
+++ docs/static/js/main.js
@@ -0,0 +1,78 @@
+console.log("If you're reading this: the code worked, Morty!");
+import * as navAndFooterLinks from "./generateNavbarAndFooter.js";
+
+// This is a pseudo-dictionary; the key is:
+// "Page name": "corresponding HTML file path"
+//
+// If the path is null, the link will appear as disabled.
+// const navbarPagesDict = [
+//     ["Home","index.html"], 
+//     ["Resume","resume.html"],
+//     ["Porfolio","porfolio.html"],
+//     ["About", null]
+// ];
+
+// function createNavbarContainer(nameOfParentElement) {
+//     let parentElement = document.getElementById(nameOfParentElement);
+
+//     let navbarContainer = document.createElement("div");
+//     navbarContainer.id = "navbarContainer";
+//     navbarContainer.className = "container-fluid";
+//     parentElement.appendChild(navbarContainer);
+// };
+
+// function createNavbarNavElement(nameOfParentElement) {
+//     let parentElement = document.getElementById(nameOfParentElement);
+
+//     let navbarDiv = document.createElement("nav");
+//     navbarDiv.id = "navbar";
+//     navbarDiv.style = `--bs-breadcrumb-divider: "//";`;
+//     navbarDiv.setAttribute("aria-label", "breadcrumb");
+//     parentElement.appendChild(navbarDiv);
+// };
+
+// function createNavbarOrderedList(nameOfParentElement) {
+//     let parentElement = document.getElementById(nameOfParentElement);
+
+//     let navbarOrderedList = document.createElement("ol");
+//     navbarOrderedList.id = "navbar-ol-element"
+//     navbarOrderedList.className = "breadcrumb";
+//     parentElement.appendChild(navbarOrderedList);
+// };
+
+// function createNavbarListElement(nameOfParentElement, text, link) {
+//     if (link != null) {
+//         let parentElement = document.getElementById(nameOfParentElement);
+
+//         let navbarListElement = document.createElement("li");
+//         navbarListElement.className = "breadcrumb-item";
+        
+//         let navbarListElementLink = document.createElement("a");
+//         navbarListElementLink.href = link
+//         navbarListElementLink.textContent = text;
+
+//         parentElement.appendChild(navbarListElement);
+//         navbarListElement.appendChild(navbarListElementLink);
+//     }
+
+//     else {
+//         let parentElement = document.getElementById(nameOfParentElement);
+
+//         let navbarListElement = document.createElement("li");
+//         navbarListElement.className = "breadcrumb-item disabled";
+
+//         let navbarListElementLink = document.createElement("a");
+//         navbarListElementLink.textContent = text;
+        
+//         parentElement.appendChild(navbarListElement);
+//         navbarListElement.appendChild(navbarListElementLink);
+//     };
+// };
+
+navAndFooterLinks.createNavbarContainer("mainContainer");
+navAndFooterLinks.createNavbarNavElement("navbarContainer");
+navAndFooterLinks.createNavbarOrderedList("navbar");
+
+navAndFooterLinks.navbarPagesDict.forEach(function(key) {
+    navAndFooterLinks.createNavbarListElement("navbar-ol-element", key[0], key[1]);
+});
